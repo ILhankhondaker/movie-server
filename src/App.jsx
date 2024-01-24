481ab0c8c8e721261d8f64ecf0eb95189cc4b2e2
ILhankhondaker <ilhankhondaker@gmail.com>
2024-01-25 05:00:06 +0600

    project setup  & install tailwind css & set theme

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-center text-green-500 text-3xl font-bold">Bisimillah Start a new project </h1>
+
+    </>
+  )
+}
+
+export default App
